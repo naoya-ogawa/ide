e957262fa935cfd4f1fd58b3100b618f1a9266ab
naoya-ogawa <root@localhost.localdomain>
2014-09-14 14:44:14 +0900

    golang

--- update.sh
+++ update.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 vim -u ~/.vimrc -i NONE -c "try | NeoBundleUpdate! | finally | q! | endtry" -e -s -V1
+
+export GOROOT="/usr/lib64/golang"
+export GOPATH=$GOROOT/packages
+export PATH=$PATH:$GOROOT/bin
+export PATH=$PATH:$GOPATH/bin
+
+go get github.com/nsf/gocode
 echo ''
